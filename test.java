7f5d18816a3f3791859f6a72012a158c2bab3a52
neongasyy <72734599+neongasyy@users.noreply.github.com>
2023-10-16 17:18:31 +0800

    Test.java

--- test.java
+++ test.java
@@ -1,1 +1,6 @@
 /*Tester File - If you can see this on your local comp then repo works*/
+class Test {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!"); 
+    }
+}
