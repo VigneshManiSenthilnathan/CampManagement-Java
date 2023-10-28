e4d736a8fc7239822e6edf4ed8b611fd4384dd02
Alan Ye Yansong <72734599+neongasyy@users.noreply.github.com>
2023-10-28 22:42:32 +0800

    Deleted test.java

--- test.java
+++
@@ -1,6 +0,0 @@
-/*Tester File - If you can see this on your local comp then repo works*/
-class Test {
-    public static void main(String[] args) {
-        System.out.println("Hello, World!"); 
-    }
-}
